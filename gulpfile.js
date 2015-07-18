5d1b64256bd2eae25e5c42466a4c9a05b79b4af7
yukihir0 <yukihiro.cotori@gmail.com>
2015-07-18 15:32:33 +0900

    add package gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var packager = require('electron-packager');
 
 gulp.task("babel", function() {
   return gulp.src("./src/**/*.js")
@@ -21,6 +22,21 @@ gulp.task("copy_css", function() {
 
 gulp.task("watch", function() {
   gulp.watch("./src/*.js", ["babel", "copy"])
+});
+
+gulp.task('package', ["babel", "copy"], function (done) {
+  packager({
+    dir: ".",
+    name: "electron-es6-angular",
+    arch: "x64",
+    platform: "darwin",
+    out: "release/darwin",
+    asar: true,
+    version: "0.30.0"
+  }, function (err, path) {
+    console.log(err);
+    done();
+  });
 });
 
 gulp.task("default", ["babel", "copy"]);
